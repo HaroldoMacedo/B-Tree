78a6e1a443b6db5dea4e715fb5cd950b380c3503
Haroldo Macedo <haroldo.macedo@gmail.com>
2022-05-10 06:32:10 +0100

    Forgotten test improvement.

--- B-Tree Object Node & Key/test/algorithm/btree/test/TestAddStringKey.java
+++ B-Tree Object Node & Key/test/algorithm/btree/test/TestAddStringKey.java
@@ -9,16 +9,16 @@ public class TestAddStringKey {
 //    String[] keys = { "Julia", "Bruno", "Ricardo", "Ana Paula", "Haroldo" };
     String[] keys = { "10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "Julia", "Bruno", "Ricardo", "Ana Paula", "Haroldo" };
 
-    BTree b = new BTree(3);
-    add(b, keys);
+    BTreePrintNodes bp = new BTreePrintNodes(3);
+    add(bp, keys);
     
-    keyInTree(b, new StringKey("1"));
-    keyInTree(b, new StringKey("7"));
-    keyInTree(b, new StringKey("61"));
-    keyInTree(b, new StringKey("10"));
-    keyInTree(b, new StringKey("11"));
-    keyInTree(b, new StringKey("Julia"));
-    keyInTree(b, new StringKey("julia"));
+    bp.keyInTree(new StringKey("1"));
+    bp.keyInTree(new StringKey("7"));
+    bp.keyInTree(new StringKey("61"));
+    bp.keyInTree(new StringKey("10"));
+    bp.keyInTree(new StringKey("11"));
+    bp.keyInTree(new StringKey("Julia"));
+    bp.keyInTree(new StringKey("julia"));
   }
 
   static void add(BTree b, String[] args) {
@@ -27,9 +27,4 @@ public class TestAddStringKey {
     }
 
   }
-
-  static void keyInTree(BTree b, StringKey key) {
-    System.out.printf("Key %s is %s in tree.\n", key, (b.isKeyInTree(key) ? "" : "NOT"));
-  }
-
 }
